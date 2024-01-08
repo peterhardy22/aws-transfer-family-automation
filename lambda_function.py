2eaa0128f5186dbfd1aa3d3dd47bcf793657bccd
Peter Hardy <peterphardy@gmail.com>
2024-01-08 15:36:18 -0600

    variables added

--- lambda_function.py
+++ lambda_function.py
@@ -17,3 +17,20 @@ try:
 except:
     from urllib3.exceptions import InsecureRequestWarning
     import urllib3
+
+
+session = new_aws_session(os.environ["ACCOUNT_NUMBER"])
+orchestration_secret_name: str = os.environ["ORCHESTRATION_SECRET"]
+sftp_secret_name: str = os.environ["SFTP_SECRET"]
+sftp_table_name: str = os.environ["SFTP_TABLE_NAME"]
+region: str = os.environ["AWS_REGION"]
+
+dynamodb_resource = session.resource("dynamodb", region_name=region)
+dynamodb_table_resource = dynamodb_resource.Table(sftp_table_name)
+s3_resource = session.resource("s3")
+s3_client = session.client("s3")
+sts_client = session.client("secretsmanager", region_name=region)
+
+
+def lambda_handler(event: dict, context: dict) -> None:
+    pass
