51c6bb2ac63fca05bc6674287d27071be335b7c7
Peter Hardy <peterphardy@gmail.com>
2024-02-08 11:10:38 -0600

    Started block for sftp_admin_role but need to finish sftp server first

--- sftp-server-system/Terraform/main.tf
+++ sftp-server-system/Terraform/main.tf
@@ -2,17 +2,56 @@ provider "aws" {
     region = var.region
 }
 
-data "aws_vpcs" "sftp_vpc" {
-
+data "aws_subnets" "sftp_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpcs.sftp_vpc.id]
+  }
 }
 
-resource "aws_transfer_server" "example" {
+data "aws_vpc" "sftp_vpc" {
+    filter {
+      name   = "tag:Name"
+      values = [var.vpc_name]
+  }
+}
+
+resource "aws_transfer_server" "sftp_server" {
   endpoint_type = "VPC"
 
   endpoint_details {
-    subnet_ids = data.aws_vpcs.sftp_vpc.subnet_ids
+    subnet_ids = data.aws_vpcs.sftp_subnets.ids
     vpc_id     = data.aws_vpcs.sftp_vpc.id
   }
 
+  logging_role = data.terraform_remote_state.sftp_admin_role.outputs.sftp_admin_role_arn
   protocols   = ["SFTP"]
+
+  depends_on = [aws_iam_role.sftp_admin_role]
+}
+
+resource "aws_iam_role" "sftp_admin_role" {
+  name = "sftp-admin-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+            "iam:GetRole",
+            "iam:PassRole"
+        ]
+        Effect   = "Allow"
+        Sid      = ""
+        Resource = [
+                "arn:aws:iam::############:role/sftp-class1-role",
+                "arn:aws:iam::############:role/sftp-standard-role"
+            ]
+      },
+    ]
+  })
+}    
+
+output "sftp_admin_role_arn" {
+  value = aws_iam_role.sftp_admin_role.arn
 }
