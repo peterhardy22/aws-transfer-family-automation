c53d3b860c7eecfcf6e87fe336af1b70d51dc73c
Peter Hardy <peterphardy@gmail.com>
2024-02-12 12:56:32 -0600

    user pool configured

--- sftp-server-system/Terraform/cognito.tf
+++ sftp-server-system/Terraform/cognito.tf
@@ -3,5 +3,17 @@ provider "aws" {
 }
 
 resource "aws_cognito_user_pool" "cognito_user_pool" {
-  name = ""
+  name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-cognito-userpool-servicenow"
+  
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+
+  username_configuration {
+    case_sensitive = false
+  }
+
 }
