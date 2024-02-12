074b169208972d1da47c951a827df928fa9fd567
Peter Hardy <peterphardy@gmail.com>
2024-02-12 12:41:02 -0600

    Added more region and account specific values

--- sftp-server-system/Terraform/terraform.tfvars
+++ sftp-server-system/Terraform/terraform.tfvars
@@ -1,3 +1,4 @@
 region = "us-east-2"
+region_prefix = "ue2"
 subnet_ids = [ "subnet-#########", "subnet-#########", "subnet-#########" ]
 vpc_name = "prod-ue2-vpc-a"
--- sftp-server-system/Terraform/variables.tf
+++ sftp-server-system/Terraform/variables.tf
@@ -1,6 +1,25 @@
+variable "aws_environment" {
+  description = "Value of the AWS account"
+  type        = string
+  default     = "dev"
+}
+
 variable "region" {
   description = "Value of the AWS Region"
   type        = string
+  default     = "us-east-2"
+}
+
+variable "region_prefix" {
+  description = "Value of AWS Region abbreviated"
+  type        = string
+  default     = "ue2"
+}
+
+variable "resource_name" {
+  description = "Determines specific app this is dedicated to"
+  type        = string
+  default     = "sftp"
 }
 
 variable "subnet_ids" {
