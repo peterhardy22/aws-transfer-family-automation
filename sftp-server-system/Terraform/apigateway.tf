edee3ed0ab8629f05002632fbd535fed7722d85b
Peter Hardy <peterphardy@gmail.com>
2024-02-13 16:19:38 -0600

    Startong API Gateway provisioning

--- sftp-server-system/Terraform/apigateway.tf
+++ sftp-server-system/Terraform/apigateway.tf
@@ -2,3 +2,6 @@ provider "aws" {
     region = var.region
 }
 
+resource "aws_api_gateway_rest_api" "sftp_api_gateway" {
+    name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-api-gateway"
+}
