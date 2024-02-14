48982f5ae92b45edeb49391b5ec7702147a1d87d
Peter Hardy <peterphardy@gmail.com>
2024-02-14 15:17:26 -0600

    API Gateway endpoint configuration

--- sftp-server-system/Terraform/apigateway.tf
+++ sftp-server-system/Terraform/apigateway.tf
@@ -3,5 +3,11 @@ provider "aws" {
 }
 
 resource "aws_api_gateway_rest_api" "sftp_api_gateway" {
+
     name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-api-gateway"
+
+    endpoint_configuration {
+      types            = ["PRIVATE"]
+      vpc_endpoint_ids = [data.aws_vpcs.sftp_vpc.id]
+  }
 }
