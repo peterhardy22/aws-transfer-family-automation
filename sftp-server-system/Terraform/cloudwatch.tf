1eef5bac9fc335c71cb666a5fe0271f18115ee4d
Peter Hardy <peterphardy@gmail.com>
2024-02-16 14:51:39 -0600

    CloudWatch good to go

--- sftp-server-system/Terraform/cloudwatch.tf
+++ sftp-server-system/Terraform/cloudwatch.tf
@@ -9,3 +9,19 @@ resource "aws_cloudwatch_log_group" "sftp_user_creation_lambda_log_group" {
     depends_on = [aws_lambda_function.sftp_user_creation_lambda_function]
 }
 
+resource "aws_cloudwatch_metric_alarm" "sftp_user_creation_lambda_error_alarm" {
+  alarm_name          = "${aws_lambda_function.sftp_user_creation_lambda_function.function_name}-cloudwatch-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 120
+  statistic           = "Sum"
+  threshold           = 1
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Alarm triggered when Lambda function encounters errors"
+  alarm_actions       = [aws_sns_topic.sftp_sns_topic.arn]
+  dimensions = {
+    FunctionName = aws_lambda_function.sftp_user_creation_lambda_function.function_name
+  }
+}
