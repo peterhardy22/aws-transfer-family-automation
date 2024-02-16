71bcc8df509b3def5068f7ba3f3e766ba5dff7b1
Peter Hardy <peterphardy@gmail.com>
2024-02-16 10:51:12 -0600

    Completed authorizer and resource for api gateway

--- sftp-server-system/Terraform/apigateway.tf
+++ sftp-server-system/Terraform/apigateway.tf
@@ -26,3 +26,18 @@ resource "aws_api_gateway_rest_api" "sftp_api_gateway" {
       }]
     })
 }
+
+resource "aws_api_gateway_authorizer" "sftp_api_gateway_authorizer" {
+    name = "Snow-Pawtrol"
+    provider_arns = data.terraform_remote_state.snow_user_pool.outputs.snow_user_pool_arn
+    rest_api_id = aws_api_gateway_rest_api.sftp_api_gateway.id
+    type = "COGNITO_USER_POOLS"
+
+    depends_on = [aws_cognito_user_pool.snow_user_pool]
+}
+
+resource "aws_api_gateway_resource" "sftp_api_gateway_resource" {
+    parent_id = aws_api_gateway_rest_api.sftp_api_gateway.root_resource_id
+    path_part = var.resource_name
+    rest_api_id = aws_api_gateway_rest_api.sftp_api_gateway.id
+}
