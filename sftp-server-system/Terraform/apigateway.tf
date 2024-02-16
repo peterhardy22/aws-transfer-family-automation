ec40e2d90a16f405291b770494c1bcfbd6298591
Peter Hardy <peterphardy@gmail.com>
2024-02-16 08:52:49 -0600

    Rest API provisioned

--- sftp-server-system/Terraform/apigateway.tf
+++ sftp-server-system/Terraform/apigateway.tf
@@ -9,5 +9,20 @@ resource "aws_api_gateway_rest_api" "sftp_api_gateway" {
     endpoint_configuration {
       types            = ["PRIVATE"]
       vpc_endpoint_ids = [data.aws_vpcs.sftp_vpc.id]
-  }
+    }
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [{
+        Effect = "Allow"
+        Action = "execute-api:Invoke"
+        Principal = "*"
+        Resource = "arn:aws:execute-api:us-east-2:############:*/*/*/*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceVpc" = data.aws_vpcs.sftp_vpc.id
+          }
+        }
+      }]
+    })
 }
