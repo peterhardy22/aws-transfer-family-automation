6d613f68484952e4c818f2716f06965c482eb68a
Peter Hardy <peterphardy@gmail.com>
2024-02-18 14:46:58 -0600

    Beginning provisioning

--- sftp-server-system/Terraform/dynamodb.tf
+++ sftp-server-system/Terraform/dynamodb.tf
@@ -3,5 +3,9 @@ provider "aws" {
 }
 
 resource "aws_dynamodb_global_table" "sftp_global_user_table" {
-  
+    name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-global-users-table"
+    
+    replica {
+      region_name = "us-east-1"
+    }
 }
