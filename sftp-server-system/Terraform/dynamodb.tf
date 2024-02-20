fd82fde100e08c3e55ce216a7d161ee795397c13
Peter Hardy <peterphardy@gmail.com>
2024-02-20 09:04:29 -0600

    80% complete with dynamodb

--- sftp-server-system/Terraform/dynamodb.tf
+++ sftp-server-system/Terraform/dynamodb.tf
@@ -2,14 +2,34 @@ provider "aws" {
     region = var.region
 }
 
-resource "aws_dynamodb_table" "sftp_global_user_table" {
-    name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-global-users-table"
+resource "aws_dynamodb_table" "sftp_user_table" {
+    name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-ue2-users-table"
     billing_mode = "PAY_PER_REQUEST"
+    hash_key = "user_name"
+    stream_enabled = true
     stream_view_type = "NEW_AND_OLD_IMAGES"
+    table_class = "STANDARD_INFREQUENT_ACCESS"
+
+    point_in_time_recovery {
+        enabled = true
+    }
+
+    server_side_encryption {
+        enabled = true
+    }
 
     attribute {
         name = "user_name"
         type = "S"
+    }
+}
+
+resource "aws_dynamodb_global_table" "sftp_global_user_table" {
+    depends_on = [sftp_user_table_ue2, sftp_user_table_ue1]
+    name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-global-users-table"
+    
+    replica {
+      region_name = "us-east-2"
     }
 
     replica {
