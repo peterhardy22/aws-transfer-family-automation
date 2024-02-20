bf388e114ea2134573574eee58f7b7c7014fa630
Peter Hardy <peterphardy@gmail.com>
2024-02-19 19:41:13 -0600

    Cont'd provisioning of table

--- sftp-server-system/Terraform/dynamodb.tf
+++ sftp-server-system/Terraform/dynamodb.tf
@@ -2,9 +2,16 @@ provider "aws" {
     region = var.region
 }
 
-resource "aws_dynamodb_global_table" "sftp_global_user_table" {
+resource "aws_dynamodb_table" "sftp_global_user_table" {
     name = "${var.resource_name}-${var.aws_environment}-${var.region_prefix}-global-users-table"
-    
+    billing_mode = "PAY_PER_REQUEST"
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+
+    attribute {
+        name = "user_name"
+        type = "S"
+    }
+
     replica {
       region_name = "us-east-1"
     }
